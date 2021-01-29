656f66a1e19b9ba59c8c756a52df6bd6b563bf5e
Dmytro Shults <dmytroshults@gmail.com>
2021-01-29 16:07:33 +0100

    Improve .T implementation through @property

--- LOGREG/my_numpy.py
+++ LOGREG/my_numpy.py
@@ -3,10 +3,9 @@ class Array:
     def __init__(self, data):
         self.data = data
         self.len = len(data)
-        self.transpose()
-    
+
     def __getitem__(self, item):
-         return self.data[item]
+        return self.data[item]
 
     def __iter__(self):
         return iter(self.data)
@@ -15,7 +14,7 @@ class Array:
         return self.len
 
     def __str__(self):
-        return str(self.data)        
+        return str(self.data)
 
     def math(self, other, f):
         """Peform math operation [f] (+ - * / **) on [self] and [other].
@@ -72,7 +71,7 @@ class Array:
     def __isub__(self, other):
         """self -= other"""
         return self.__sub__(other)
-    
+
     def __neg__(self):
         """-self"""
         return self.__mul__(-1)
@@ -86,24 +85,32 @@ class Array:
         except:
             for i in range(len(self.data)):
                 self.data[i] = dtype(self.data[i])
-        self.transpose()
         return self
 
     def dot(self, other):
         return Numpy.dot(self, other)
 
-    def transpose(self):
-        self.T = []
+    @property
+    def T(self):
         try:
-            for j in range(len(self.data[0])):
+            return self._transposed
+        except AttributeError:
+            self.T = self.data
+            return self._transposed
+
+    @T.setter
+    def T(self, data):
+        self._transposed = []
+        try:
+            for j in range(len(data[0])):
                 new = []
-                for i in range(len(self.data)):
-                    new.append(self.data[i][j])
-                self.T.append(new)
+                for i in range(len(data)):
+                    new.append(data[i][j])
+                self._transposed.append(new)
         # if only one row
         except:
-            for i in range(len(self.data)):
-                self.T.append([self.data[i]])        
+            for i in range(len(data)):
+                self._transposed.append([data[i]])
 
 
 class Numpy:
@@ -122,7 +129,7 @@ class Numpy:
         [ a b           [ w x           [ a∗w+b∗y a∗x+b∗z
           c d       *     y z ]     =     c∗w+d∗y c∗x+d∗z
           e f ]                           e∗w+f∗y e∗x+f∗z ]
-        
+
         Keyword arguments:
         A - list or Array with numbers (1 or 2 dimensional)
         B - list or Array with numbers (1 or 2 dimensional)
@@ -133,9 +140,9 @@ class Numpy:
         # if both A and B are 2D
         try:
             B_columns = len(B[0])
-            for i in range(A_rows): 
+            for i in range(A_rows):
                 dot = []
-                for j in range(B_columns): 
+                for j in range(B_columns):
                     n = 0
                     for k in range(B_rows):
                         n += A[i][k] * B[k][j]
@@ -151,7 +158,7 @@ class Numpy:
                 return dot
             # if A is 2D and B is 1D
             else:
-                for i in range(A_rows): 
+                for i in range(A_rows):
                     dot = 0
                     for k in range(B_columns):
                         dot += A[i][k] * B[k]
@@ -178,7 +185,7 @@ class Numpy:
 
         Example:
             np.insert(X, 0, 1, axis=1)
-        
+
         Keyword arguments:
         arr -- Array to insert into
         obj -- position in Array
@@ -188,9 +195,11 @@ class Numpy:
         try:
             dtype = type(arr[0][obj])
         except TypeError:
-            import sys, traceback
+            import sys
+            import traceback
             tb = traceback.format_exc()
-            sys.exit(f"{tb}\nCannot insert {values} into array, because it's one-dimensional")
+            sys.exit(
+                f"{tb}\nCannot insert {values} into array, because it's one-dimensional")
 
         copy = []
         for X in arr:
@@ -200,5 +209,5 @@ class Numpy:
         return Array(copy)
 
     @staticmethod
-    def zeros(shape):        
+    def zeros(shape):
         return Array([0] * shape).astype(float)
